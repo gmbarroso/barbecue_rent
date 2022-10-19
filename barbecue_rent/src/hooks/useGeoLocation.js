1bca4c6b26277043ecf62230df05be045956e783
gmbarroso <guilherme.barroso@zenvia.com>
2022-10-19 20:59:32 -0300

    add useGeoLocation hook

---
+++ barbecue_rent/src/hooks/useGeoLocation.js
@@ -0,0 +1,38 @@
+
+
+import { useState, useEffect } from 'react'
+
+const useGeolocation = (options ={}) => {
+  const [ error, setError ] = useState()
+  const [ latitude, setLatitude ] = useState()
+  const [ longitude, setLongitude ] = useState()
+  
+  const handleSuccess = position => {
+    const { latitude, longitude } = position.coords
+      setLatitude(latitude)
+      setLongitude(longitude)
+  }
+    
+  const handleError = err => {
+    setError(err.message)
+  }
+    
+  useEffect(() => {
+    if(!navigator.geolocation) {
+      setError('Location not found')
+      return
+    }
+    
+    // navigator.geolocation.getCurrentPosition(handleSuccess, handleError)
+    const watcher = navigator.geolocation.watchPosition(handleSuccess, handleError)
+    return () => navigator.geolocation.clearWatch(watcher)
+  }, [options])
+
+  return {
+    latitude,
+    longitude,
+    error
+  }
+}
+
+export default useGeolocation
